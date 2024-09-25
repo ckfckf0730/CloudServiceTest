71b3261135f9e39827f4ac548d5a4ebcfbd9112c
ckfckf0730 <39393418+ckfckf0730@users.noreply.github.com>
2024-09-25 15:11:54 +0900

    Update Role management function.

--- CloudServiceTest/Controllers/AdminController.cs
+++ CloudServiceTest/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace CloudServiceTest.Controllers
@@ -10,18 +11,21 @@ namespace CloudServiceTest.Controllers
     public class AdminController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AdminController(UserManager<ApplicationUser> userManager)
+        public AdminController(UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager)
         {
 			_userManager = userManager;
+            _roleManager = roleManager;
 
-		}
+        }
 
         [HttpGet]
         public async Task<ActionResult> Admin()
         {
             int pageNumber = 1; // The page number you want to retrieve
-            int pageSize = 20;  // Number of items per page
+            int pageSize = 10;  // Number of items per page
 
             var userList = _userManager.Users.Skip((pageNumber - 1) * pageSize)
                                         .Take(pageSize)
@@ -32,5 +36,45 @@ namespace CloudServiceTest.Controllers
             model.Users = userList;
             return View(model);
         }
-    }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserRoles(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return Json(new { message = "Can't find user: " + userId });
+            }
+            var roles = _userManager.GetRolesAsync(user).Result;
+            return Json(roles); 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRoles()
+        {
+            var roles = await _roleManager.Roles.ToListAsync() ;
+
+            var json = Json(roles);
+			return json;
+        }
+
+		public async Task<IActionResult> AddUserRole(string userName, string roleName)
+		{
+			var user = await _userManager.FindByNameAsync(userName);
+			if (user == null)
+			{
+				return Json(new { message = "Can't find user: " + userName });
+			}
+
+			var isRole = await _userManager.IsInRoleAsync(user, roleName);
+			if (!isRole)
+			{
+				var result = await _userManager.AddToRoleAsync(user, roleName);
+                return Json(new { isSucceeded = result.Succeeded });
+			}
+			
+            return Json(new { isSucceeded = false });
+		}
+
+	}
 }
